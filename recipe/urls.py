179c83f988ce0eebc1df288027380d2e772c5d16
Lee Copeman <leepcopeman@gmail.com>
2024-10-02 13:57:13 +0000

    Added recipe_feature view

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
+    path('<slug:slug>/', views.recipe_feature, name='recipe_feature'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -7,3 +7,25 @@ class RecipeList(generic.ListView):
     template_name = "recipe/index.html"
     paginate_by = 3
 
+def recipe_feature(request, slug):
+    """
+    Display an individual :model:`recipe.Recipe`.
+
+    **Context**
+
+    ``recipe``
+        An instance of :model:`recipe.Recipe`.
+
+    **Template:**
+
+    :template:`recipe/recipe_feature.html`
+    """
+
+    queryset = Recipe.objects.filter(status=1)
+    recipe = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "recipe/recipe_feature.html",
+        {"recipe": recipe},
+    )
