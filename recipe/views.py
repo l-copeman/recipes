d23319710106989d73ba7ecf21a24b204e467faa
Lee Copeman <leepcopeman@gmail.com>
2024-10-04 10:23:33 +0000

    Install crispy forms and create comment form

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
+from .forms import CommentForm
 
 class RecipeList(generic.ListView):
     model = Recipe
@@ -24,11 +25,21 @@ def recipe_feature(request, slug):
     queryset = Recipe.objects.filter(status=1)
     recipe = get_object_or_404(queryset, slug=slug)
     comments = recipe.comments.all().order_by("-created_on")
+    comment_form = CommentForm()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.recipe = recipe
+        comment.save()
+
 
     return render(
         request,
         "recipe/recipe_feature.html",
         {"recipe": recipe,
         "comments": comments,
+        "comment_form": comment_form,
         },
     )
